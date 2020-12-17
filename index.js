3b65a110228e5c674f9ef9276f6f49a1e8e52618
Desktop <benneffect@gmail.com>
2020-12-17 16:06:22 -0700

    Solved all Tasks, test suite active.

--- index.js
+++ index.js
@@ -169,9 +169,23 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
- }
+  class Student extends Lambdasian {
+    constructor(obj){
+      super(obj);
+      this.previousBackground=obj.previousBackground;
+      this.className=obj.className;
+      this.favSubjects=obj.favSubjects
+    }
+    listSubjects(){
+      return `${this.favSubjects}`;
+    }
+    PRAssignment(subject){
+      return `${this.name} has submitted a PR for ${subject}`
+    }
+    sprintChallenge(subject){
+      return `${this.name} has begun sprint challenge on ${subject}`
+    }
+  }
   
   /*
     TASK 6
@@ -186,8 +200,18 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+     constructor(obj){
+     super(obj);
+     this.gradClassName=obj.gradClassName;
+     this.favInstructor=obj.favInstructor;
+     }
+     standUp(channel){
+       return `${this.name} announces to ${channel}, @channel standy times! `
+     }
+     debugsCode(student,subject){
+       return `${this.name} debugs ${student.name}'s code on ${subject}`
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
